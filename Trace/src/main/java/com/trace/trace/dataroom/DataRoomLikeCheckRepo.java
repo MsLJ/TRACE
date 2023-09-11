d728287c65bc606cd5c1b26a6b12a5191f47596a
MsLJ <134290720+MsLJ@users.noreply.github.com>
2023-09-11 14:41:43 +0900

    Delete Trace directory

--- Trace/src/main/java/com/trace/trace/dataroom/DataRoomLikeCheckRepo.java
+++
@@ -1,9 +0,0 @@
-package com.trace.trace.dataroom;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DataRoomLikeCheckRepo extends CrudRepository<DataRoomLikeCheck, Integer> {
-	public abstract boolean existsByDrNoAndLcId(Integer no, String id);
-}
