c168a9db27b154cda51661a9b2ff3910db17ac89
MsLJ <134290720+MsLJ@users.noreply.github.com>
2023-09-12 14:57:39 +0900

    Add files via upload

--- Trace/src/main/resources/static/js/detailck.js
+++ Trace/src/main/resources/static/js/detailck.js
@@ -39,8 +39,8 @@ function memberupdateck(f) {
 
 
 function memberbye() {
-	var really = prompt("탈퇴하시겠습니까? 탈퇴를 입력해주세요");
-	if (really == "탈퇴") {
+	var really = prompt("휴면 활성화하시겠습니까? 휴면를 입력해주세요");
+	if (really == "휴면") {
 		location.href = "member.delete";
 
 	}
